bb9e915c9ec4b2a29994cf71b487cde55d7962c7
Lex Nousagi <alexmiyin@gmail.com>
2023-01-11 21:57:46 +0200

    chore(dev): add prettier

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/** @type {import('prettier').Config} */
+const options = {
+  arrowParens: "avoid",
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  semi: false,
+  tabWidth: 2,
+  trailingComma: "none",
+  printWidth: 80,
+};
+
+module.exports = options;
